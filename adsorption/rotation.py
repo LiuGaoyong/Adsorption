847f3279ce7e04728176671f180fc8540a737c02
LiuGaoyong <liugaoyong_88@163.com>
2025-07-27 15:49:48 +0800

    update kabsch function, and test passed

--- adsorption/rotation.py
+++ adsorption/rotation.py
@@ -39,3 +39,57 @@ def rotate(
     ), "Center must be a 3D vector."
 
     return rotation.apply(points - center) + center
+
+
+def kabsch(
+    A: npt.ArrayLike,
+    B: npt.ArrayLike,
+) -> tuple[Rot, np.ndarray, float]:
+    """Find rotation matrix and translation vector for convert A into B.
+
+    This method solves and returns the R & t in
+        A = R.apply(B) + t
+        where A & B are Nx3 matrices for point coordinates,
+            R is the scipy Rotation object, and
+            t is the 3D translation vector.
+
+    See details:
+        https://nghiaho.com/?page_id=671
+        https://github.com/nghiaho12/rigid_transform_3D
+        https://blog.csdn.net/u012836279/article/details/80203170
+        https://blog.csdn.net/u012836279/article/details/80351462
+    Advanced Methods:
+        https://github.com/mammasmias/IterativeRotationsAssignments
+        https://pubs.acs.org/doi/10.1021/acs.jcim.2c01187
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]:
+            The first item is rotation matrix which is the instance
+                instance of scipy.spatial.transform.Rotation.
+            The second item is translation vector.
+            The third item is the RMSD value.
+    """
+    A, B = np.asarray(A, dtype=float), np.asarray(B, dtype=float)
+    if A.ndim != 2 or B.ndim != 2:
+        raise ValueError("Arrays must be of 2D arrays.")
+    if A.shape[-1] != 3 or B.shape[-1] != 3:
+        raise ValueError("Array elements must be 3D vectors.")
+    assert isinstance(A, np.ndarray) and isinstance(B, np.ndarray)
+
+    # find mean/centroid
+    centroid_A: np.ndarray = np.mean(A, axis=0)
+    centroid_B: np.ndarray = np.mean(B, axis=0)
+
+    # subtract mean
+    # NOTE: doing A -= centroid_A will modifiy input!
+    A, B = A - centroid_A, B - centroid_B
+    assert np.all(np.abs(np.mean(A, axis=0)) < 1e-5)
+    assert np.all(np.abs(np.mean(B, axis=0)) < 1e-5)
+
+    rotation, rmsd = Rot.align_vectors(
+        A,
+        B,
+        weights=None,
+        return_sensitivity=False,
+    )
+    return rotation, centroid_A - centroid_B, rmsd
--- tests/test_rotation.py
+++ tests/test_rotation.py
@@ -1,14 +1,13 @@
-from random import random
-
+import numpy as np
 import pytest
 from ase.build import molecule
 
-from adsorption.rotation import Rot, rotate
+from adsorption.rotation import Rot, kabsch, rotate
 
 
 def test_rotation() -> None:  # noqa: D103
     ben = molecule("C6H6")
-    points = ben.positions + [random()] * 3
+    points = ben.positions + np.random.random(3)
     center = points.mean(axis=0)
     print(f"Points center: {center}")
 
@@ -27,3 +26,19 @@ def test_rotation() -> None:  # noqa: D103
     rotated = rotate(points, rotation)
     print(f"Rotated points around center point:\n{rotated}")
     assert pytest.approx(rotated.mean(axis=0)) == center
+
+
+def test_kabsch() -> None:  # noqa: D103
+    B = molecule("C6H6").positions + np.random.random(3)
+    R, T = Rot.random(), np.random.random(3)
+
+    A = rotate(B, R) + T
+    assert pytest.approx((A - T).mean(axis=0)) == B.mean(axis=0)
+
+    R0, T0, rmsd = kabsch(A, B)
+    assert isinstance(T0, np.ndarray)
+    assert pytest.approx(T0) == T
+    assert isinstance(R0, Rot)
+    assert pytest.approx(R0.as_matrix()) == R.as_matrix()
+    assert rmsd <= 1e-5
+    assert pytest.approx(rotate(B, R0) + T0) == A
