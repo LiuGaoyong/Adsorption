8a66bc60e427cd965e3a4106b3af361f9e09d845
LiuGaoyong <liugaoyong_88@163.com>
2025-07-27 18:06:30 +0800

    add adsorbate test passed, it may be used

--- adsorption/interface.py
+++ adsorption/interface.py
@@ -7,15 +7,16 @@ from ase.data import chemical_symbols as SYMBOLS
 from numpy import typing as npt
 
 from adsorption.calculator import get_calculator
+from adsorption.rotation import Rot, rotate
 
 
-def add_adsorbate(
+def add_adsorbate_and_optimize(
     atoms: Atoms,
     adsorbate: Atoms | Atom | str,
     calculator: Calculator | str = "gfnff",
     core: npt.ArrayLike | list[int] | int = 0,
 ) -> Atoms:
-    """Add an adsorbate to a surface or cluster.
+    """Add an adsorbate to a surface or cluster & Optimizition.
 
     Args:
         atoms (Atoms): The surface or cluster
@@ -34,7 +35,7 @@ def add_adsorbate(
                 2. Calculator object
             Defaults to "gfnff".
         core (npt.ArrayLike | list[int] | int, optional):
-            The central atoms (core) in the adsorbate to place at.
+            The central atoms (core) which will place at.
             Defaults to the first atom, i.e. the 0-th atom.
 
     Returns:
@@ -69,4 +70,71 @@ def add_adsorbate(
     core = [core] if isinstance(core, int) else core
     core = np.asarray(core, dtype=int)
 
+    raise NotImplementedError("Not implemented yet.")
+
     return Atoms()
+
+
+def add_adsorbate(
+    atoms: Atoms,
+    adsorbate: Atoms,
+    core: npt.ArrayLike,
+    distance: float,
+    rotation0: Rot,
+    rotation1: Rot,
+) -> Atoms:
+    """Add an adsorbate to a surface or cluster.
+
+    Args:
+        atoms (Atoms): The surface or cluster.
+        adsorbate (Atoms): The adsorbate molecule.
+        core (npt.ArrayLike): The central atoms (core) which to place at.
+            Defaults to the first atom, i.e. the 0-th atom.
+        distance (float): The distance between the COM of adsorbate and core.
+        rotation0 (Rot): The rotation of the adsorbate around core's COM.
+        rotation1 (Rot): The rotation of the adsorbate around its COM
+
+    Returns:
+        Atoms: The surface or cluster with adsorbate after optimization.
+    """
+    assert isinstance(atoms, Atoms), "Input must be of type Atoms."
+    assert isinstance(adsorbate, Atoms), "Adsorbate must be of type Atoms."
+
+    core = [core] if isinstance(core, int) else core
+    core = np.asarray(core, dtype=int).flatten()
+    assert isinstance(core, np.ndarray) and core.ndim == 1 and core.size > 0, (
+        "The core must be a 1D array-like object with at least one element."
+    )
+    assert np.all(core < len(atoms)), "The core must be within the atoms."
+    assert np.all(core >= 0), "The core must be non-negative."
+
+    distance = float(distance)
+    assert distance > 0, "Distance must be positive."
+    assert isinstance(rotation0, Rot), "Rotation0 must be of type Rot."
+    assert isinstance(rotation1, Rot), "Rotation1 must be of type Rot."
+
+    random_direction = np.random.rand(3)
+    random_direction /= np.linalg.norm(random_direction)
+    com_core = Atoms(atoms[core]).get_center_of_mass()
+    com_ads = adsorbate.get_center_of_mass()
+
+    adsorbate_positions = adsorbate.positions.copy()
+    adsorbate_positions = rotate(
+        rotation=rotation1,
+        points=adsorbate_positions,
+        center=com_ads,
+    )
+    adsorbate_positions -= com_ads  # move the COM to origin
+    adsorbate_positions += com_core + distance * random_direction
+    adsorbate_positions = rotate(
+        rotation=rotation0,
+        points=adsorbate_positions,
+        center=com_core,
+    )
+
+    return Atoms(
+        numbers=np.append(atoms.numbers, adsorbate.numbers),
+        positions=np.vstack((atoms.positions, adsorbate_positions)),
+        cell=atoms.cell,
+        pbc=atoms.pbc,
+    )
---
+++ tests/test_add_adsorbate.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+from ase import Atoms
+from ase.build import molecule
+from ase.io import read
+from scipy.spatial.distance import pdist
+from scipy.spatial.transform import Rotation as Rot
+
+from adsorption.interface import add_adsorbate, add_adsorbate_and_optimize
+
+
+@pytest.fixture(scope="module")
+def atoms() -> Atoms:  # noqa: D103
+    # return Octahedron("Cu", 10)
+    p = Path(__file__).parent / "OctCu10.xyz"
+    return read(p.__fspath__())  # type: ignore
+
+
+@pytest.mark.parametrize("adsorbate", ["CO", "H2O", "CH4", "C6H6"])
+@pytest.mark.parametrize(
+    "core",
+    [
+        [303, 334, 464],  # vertex fcc hollow
+        [303, 334],  # vertex bridge
+        303,  # vertex top
+        578,  # edge top
+        [578, 638],  # edge bridge
+        [578, 638, 596],  # edge fcc hollow
+        [607, 608, 610],  # surface fcc hollow
+        [608, 610],  # surface bridge
+        [610],  # surface top
+    ],
+)
+def test_add_adsorbate(  # noqa: D103
+    atoms,
+    adsorbate,
+    core: int | list[int],
+) -> None:
+    m: Atoms = molecule(adsorbate)
+    d = 1.7 + np.random.random() * 0.7
+    dpdist = np.asarray(pdist(m.positions))
+
+    for _ in range(10):
+        r0, r1 = Rot.random(), Rot.random()
+        ads0 = add_adsorbate(atoms, m, core, d, r0, r1)
+        _core = [core] if isinstance(core, int) else core
+        com_core0 = Atoms(ads0[_core]).get_center_of_mass()
+        com_ads0 = Atoms(ads0[-len(m) :]).get_center_of_mass()
+        dpdist0 = np.asarray(pdist(ads0.positions[-len(m) :]))
+        assert pytest.approx(dpdist0, abs=1e-7) == dpdist
+        d0 = float(np.linalg.norm(com_ads0 - com_core0))
+        assert pytest.approx(d0, abs=1e-7) == d
+
+
+@pytest.mark.parametrize("adsorbate", ["CO", "H2O", "CH4", "C6H6"])
+@pytest.mark.parametrize(
+    "core,name",
+    [
+        ([303, 334, 464], "v_fcc"),  # vertex fcc hollow
+        ([303, 334], "v_bri"),  # vertex bridge
+        (303, "v_top"),  # vertex top
+        (578, "e_top"),  # edge top
+        ([578, 638], "e_bri"),  # edge bridge
+        ([578, 638, 596], "e_fcc"),  # edge fcc hollow
+        ([607, 608, 610], "s_fcc"),  # surface fcc hollow
+        ([608, 610], "s_bri"),  # surface bridge
+        ([610], "s_top"),  # surface top
+    ],
+)
+@pytest.mark.parametrize("calculator", ["lj", "gfnff", "gfn0"])
+def test_add_adsorbate_and_optimize(  # noqa: D103
+    atoms,
+    adsorbate,
+    core: int | list[int],
+    calculator: str,
+    name: str,
+) -> None:  # noqa: D103
+    result = add_adsorbate_and_optimize(
+        atoms=atoms,
+        adsorbate=molecule(adsorbate),
+        calculator=calculator,
+        core=core,
+    )
+    result.write(f"{name}_{calculator}.xyz", format="extxyz")
